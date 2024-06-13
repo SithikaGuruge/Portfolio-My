a97a34cfbf3d97a07761259baa9dbe66d280afa0
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-14 00:30:23 +0530

    Tab-button-weight- increased

--- my-portfolio/src/app/components/TabButtton.jsx
+++ my-portfolio/src/app/components/TabButtton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function TabButtton({ active, selectTab, children }) {
-  const buttonClasses = active ? 'text-white border-b border-[#11823b]' : 'text-[#ADB7BE]';
+  const buttonClasses = active ? 'text-white border-b-4 border-[#11823b]' : 'text-[#ADB7BE]';
   
   return (
     <button onClick={selectTab}>
