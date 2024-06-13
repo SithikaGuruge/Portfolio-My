d18397617a7f09639bddff05788fe0a52b817b2f
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-13 13:07:24 +0530

    footer added

---
+++ my-portfolio/src/app/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <div className="mt-10 pt-10">
+    <hr></hr>
+    <div className="flex flex-row justify-between py-6">
+    <h1>© 2024</h1>
+    <h1>Sithika Guruge</h1>
+    <h1>All Rights Reserved.</h1>    
+    </div>
+    </div>
+  );
+};
+
+export default Footer;
